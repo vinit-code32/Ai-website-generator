67c465e7476c635d9ddf040b6aeaa666d7b86e37
vinit-code32 <vs3297879@gmail.com>
2025-11-20 01:45:33 -0800

    Add tooltip to SidebarTrigger in AppHeader

--- app/workshop/AppHeader.tsx
+++ app/workshop/AppHeader.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { SidebarTrigger } from '@/components/ui/sidebar';
-import { UserButton } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const AppHeader = () => {
   return (
     <div className='flex justify-between shadow-2xl flex-1  p-4 items-center'>
-      <SidebarTrigger/>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger><SidebarTrigger/></TooltipTrigger>
+          <TooltipContent>
+            <p>SideBar</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <UserButton/>
     </div>
   )
